9daba86742edc2fba69bafdb4b58b82612bb304d
Andrew Teck Kiat Lim <lim243@purdue.edu>
2018-07-02 11:53:10 -0400

    Extract Header

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Header = () => {
-    return (
-        <div className="expanded row header">
+  return (
+    <div className="expanded row header">
       <div className="clearfix">
         <div className="float-left logo">
           <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
@@ -18,7 +18,7 @@ const Header = () => {
         <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
       </div>
     </div>
-    )
+  )
 }
 
 export default Header
