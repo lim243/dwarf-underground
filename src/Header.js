5d993c244b565786d8e7cb428c21bec9d64a318a
Andrew Teck Kiat Lim <lim243@purdue.edu>
2018-07-02 13:25:53 -0400

    Extract navigation links from header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import NavigationLinks from './NavigationLinks';
+
 const Header = () => {
   return (
     <div className="expanded row header">
@@ -7,11 +9,7 @@ const Header = () => {
         <div className="float-left logo">
           <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
         </div>
-        <div className="float-right nav-links">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Log in</a>
-        </div>
+          <NavigationLinks />
       </div>
       <div className="headline text-center small-12 columns">
         <h1>The Dwarf Underground</h1>
