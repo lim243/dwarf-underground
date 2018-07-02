1a6c7d7a4d4ec12ec8b6c9a7bf84d0ed50b79d1f
Andrew Teck Kiat Lim <lim243@purdue.edu>
2018-07-02 13:32:34 -0400

    Extract ArticleLinks from large8 paragraph

---
+++ src/ArticleLinks.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ArticleLinks = () => {
+    return (
+        <div className="article-links">
+            <a className="article-link" href="#">
+                <i className="fa fa-comments-o"></i>
+                <span className="article-link-text">Comments</span>
+            </a>
+            <a className="article-link" href="#">
+                <i className="fa fa-share"></i>
+                <span className="article-link-text">Share Post</span>
+            </a>
+        </div>
+    )
+}
+
+export default ArticleLinks
